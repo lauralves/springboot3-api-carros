470d4e9c0d8af62525509ba919e3aa47e976c26e
lauralves <105663730+lauralves@users.noreply.github.com>
2023-01-10 14:17:07 -0300

    getCarroById implementation

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -1,10 +1,10 @@
 package com.carros.api;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +21,10 @@ public class CarrosController {
 	@GetMapping
 	public Iterable<Carro> get(){
 		return service.getCarros();
+	}
+	
+	@GetMapping ("/{id}")
+	public Optional<Carro> get(@PathVariable ("id") Long id){
+		return service.getCarroById(id);
 	}
 }
