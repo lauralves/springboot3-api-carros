65381afcdbdc99849912b981b86f26ee54119f53
lauralves <105663730+lauralves@users.noreply.github.com>
2023-01-10 15:59:24 -0300

    POST AND PUT: save - update carros

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +34,19 @@ public class CarrosController {
 	@GetMapping("/tipo/{tipo}")
 	public Iterable<Carro> getCarroByTipo(@PathVariable ("tipo") String tipo) {
 		return service.getCarroByTipo(tipo);
+	}
+	@PostMapping
+	public String post(@RequestBody Carro carro) {
+		Carro c = service.insert(carro);
+		
+		return "Carro salvo com sucesso. Id: " + c.getId();
+	}
+	
+	@PutMapping("/{id}")
+	public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+		Carro c = service.update(carro, id);
+		
+		return "Carro atualizado com sucesso! " + c.getId();
 	}
 	
 }
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class CarroService {
@@ -23,6 +24,31 @@ public class CarroService {
 		return rep.findByTipo(tipo);
 	}
 	
+	public Carro insert(Carro carro) {
+		return rep.save(carro);
+	}
+	
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possível atualizar o registro");
+		//busca o carro no db
+		Optional<Carro> carroId = getCarroById(id);
+		if (carroId.isPresent()) {
+			Carro db = carroId.get();
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id: " + db.getId());
+			
+			//atualiza o carro
+			
+			rep.save(db);
+			return db;
+		} else {
+			throw new RuntimeException("Não foi possível atualizar o registro");
+		}
+		
+		
+	}
+	
 	public List<Carro> getCarro(){
 		List<Carro> carros = new ArrayList<>();
 		
@@ -31,4 +57,5 @@ public class CarroService {
 		carros.add(new Carro(3L, "KKK"));
 		return carros;
 	}
+	
 }
